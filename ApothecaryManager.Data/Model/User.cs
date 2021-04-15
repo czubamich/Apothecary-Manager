9c0d44355fb79594ff880d78597c496bbb470469
Michael Czuba <czuba.mich@gmail.com>
2021-04-15 12:13:55 +0200

    Add User model

---
+++ ApothecaryManager.Data/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApothecaryManager.Data.Model
+{
+    [Table(name: "Users")]
+    class User
+    {
+        [Key]
+        public long ID { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public long Name { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public long Phone { get; set; }
+    }
+}
