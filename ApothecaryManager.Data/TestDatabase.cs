b71f433dd2a92806397ddea878caa44e7050e7ec
Bartosz Bożyczko <barybatle@gmail.com>
2021-04-15 14:34:40 +0200

    Added DbContext

--- ApothecaryManager.Data/TestDatabase.cs
+++ ApothecaryManager.Data/TestDatabase.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using ApothecaryManager.Data.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ApothecaryManager.Data
 {
-    class TestDatabase
+    class TestDatabaseContext : DbContext
     {
+        public TestDatabaseContext() : base()
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Drug> Drugs { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<RetailSale> RetailSales { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
