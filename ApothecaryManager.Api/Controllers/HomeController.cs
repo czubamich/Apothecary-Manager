8ffc795ed78ca3162f2b95230bb0448948960bcb
Michael Czuba <czuba.mich@gmail.com>
2021-04-30 23:44:18 +0200

    Update HomeController

--- ApothecaryManager.Api/Controllers/HomeController.cs
+++ ApothecaryManager.Api/Controllers/HomeController.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ApothecaryManager.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ApothecaryManager.WebApi.Controllers
 {
+    [Authorize]
+    [ApiController]
+    [Route("api")]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
+        [HttpGet("version")]
+        public String GetVersion()
+        {
+            throw new NotImplementedException();
+        }
 
+        [AllowAnonymous]
+        [HttpGet("now")]
+        public DateTime GetTimeNow()
+        {
+            return DateTime.Now;
+        }
     }
 }
