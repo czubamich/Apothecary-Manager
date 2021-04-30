96482cd7585ea80e6f81589f53df121dae619eb4
Michael Czuba <czuba.mich@gmail.com>
2021-04-30 23:26:56 +0200

    Update ShopDbContext.cs

--- ApothecaryData/ShopDbContext.cs
+++ ApothecaryData/ShopDbContext.cs
@@ -19,7 +19,7 @@ namespace ApothecaryManager.Data
         public DbSet<Drug> Drugs { get; set; }
         public DbSet<Inventory> Inventories { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
-        public DbSet<SalesDetail> RetailSales { get; set; }
+        public DbSet<SalesDetail> SalesDetails { get; set; }
         public DbSet<Sale> Sales { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<User> Users { get; set; }
