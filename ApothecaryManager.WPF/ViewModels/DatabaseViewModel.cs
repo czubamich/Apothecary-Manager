80b69b82ec2e747365ff423b9b66c74f8f774dec
Bartosz Bożyczko <barybatle@gmail.com>
2021-05-29 01:16:51 +0200

    Added DataGrid selected value checking

--- ApothecaryManager.WPF/ViewModels/DatabaseViewModel.cs
+++ ApothecaryManager.WPF/ViewModels/DatabaseViewModel.cs
@@ -9,11 +9,75 @@
     using Catel.Data;
     using Catel.IoC;
     using Catel.Services;
+    using ApothecaryManager.Data.Model;
+    using System.IO;
+    using System.Windows.Controls;
+    using System.Windows;
+    using System;
 
     public class DatabaseViewModel : ViewModelBase
     {
+
+        public ObservableCollection<Drug> Products { get; set; }
+
+        private string _textName;
+        public string TextName
+        {
+            get { return _textName; }
+            set
+            {
+                _textName = value;
+            }
+        }
+
+        private void DataGrid_SelectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(SelectedRecord.Name);
+            ChangeName();
+        }
+
+        private Drug _selectedRecord;
+        public Drug SelectedRecord
+        {
+            get { return _selectedRecord; }
+            set
+            {
+                _selectedRecord = value;
+                MessageBox.Show(value.Name + ", " + value.Description);
+                ChangeName();
+            }
+        }
+
+        public void ChangeName()
+        {
+            TextName = SelectedRecord.Name;
+        }
+
         public DatabaseViewModel(/* dependency injection here */)
         {
+            Products = new ObservableCollection<Drug>();
+
+            using (var reader = new StreamReader("leki.tsv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('\t');
+
+                    Products.Add(new Drug()
+                    {
+                        Name = values[0],
+                        ActiveSubstance = values[1],
+                        Unit = values[2],
+                        QuantityInPackage = values[3],
+                        Dose = values[4],
+                        IsPrescribed = values[5],
+                        Description = values[9]
+                    });
+                }
+            }
+
+            
         }
 
         public override string Title { get { return "Apothecary Manager"; } }
