7c0174f2eb9e9665832df4a53dee353471b9d023
Bartosz Bożyczko <barybatle@gmail.com>
2021-06-16 02:36:17 +0200

    Modified SaleView and ViewModel

--- ApothecaryManager.WPF/ViewModels/SaleViewModel.cs
+++ ApothecaryManager.WPF/ViewModels/SaleViewModel.cs
@@ -9,11 +9,57 @@
     using Catel.Data;
     using Catel.IoC;
     using Catel.Services;
+    using ApothecaryManager.Data.Model;
+    using System.IO;
+    using System.Windows.Forms;
 
     public class SaleViewModel : ViewModelBase
     {
+
+        public ObservableCollection<Drug> Products { get; set; }
+
+        private Drug _selectedRecord;
+        public Drug SelectedRecord
+        {
+            get { return _selectedRecord; }
+            set
+            {
+                if (_selectedRecord != value)
+                {
+                    _selectedRecord = value;
+                }
+            }
+        }
+
         public SaleViewModel(/* dependency injection here */)
         {
+            Products = new ObservableCollection<Drug>();
+
+            using (var reader = new StreamReader("leki.tsv"))
+            {
+                int i = 0;
+                string headerLine = reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('\t');
+
+                    Products.Add(new Drug()
+                    {
+                        Id = i,
+                        Name = values[0],
+                        ActiveSubstance = values[1],
+                        Unit = values[2],
+                        QuantityInPackage = values[3],
+                        Dose = values[4],
+                        IsPrescribed = values[5],
+                        Category = values[7],
+                        Price = values[8],
+                        Description = values[9]
+                    });
+                    i++;
+                }
+            }
         }
 
         public override string Title { get { return "Apothecary Manager"; } }
@@ -36,6 +82,11 @@
             // TODO: unsubscribe from events here
 
             await base.CloseAsync();
+        }
+
+        void ListBoxItem_MouseDoubleClick()
+        {
+            MessageBox.Show("XD");
         }
     }
 }
