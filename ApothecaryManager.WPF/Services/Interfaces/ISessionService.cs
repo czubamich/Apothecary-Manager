c055c7d2ae99f8075e4a7ee1a61fc4b98b40f178
Michael Czuba <czuba.mich@gmail.com>
2021-06-18 09:20:34 +0200

    Add SessionService

---
+++ ApothecaryManager.WPF/Services/Interfaces/ISessionService.cs
@@ -0,0 +1,20 @@
+﻿using ApothecaryManager.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApothecaryManager.WPF.Services.Interfaces
+{
+    interface ISessionService
+    {
+        bool LoggedIn { get; }
+        User LoggedUser { get; }
+
+        void Setup();
+        bool Login(string username, string password);
+        bool Refresh();
+        bool Logout();
+    }
+}
---
+++ ApothecaryManager.WPF/Services/SessionService.cs
@@ -0,0 +1,39 @@
+﻿using ApothecaryManager.Data.Model;
+using ApothecaryManager.WPF.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApothecaryManager.WPF.Services
+{
+    class SessionService : ISessionService
+    {
+        string RefreshToken { get; set; }
+        string AccessToken { get; set; }
+
+        public bool LoggedIn => throw new NotImplementedException();
+        public User LoggedUser => throw new NotImplementedException();
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Setup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
