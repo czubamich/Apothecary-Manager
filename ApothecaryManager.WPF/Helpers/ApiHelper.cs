b838667e6eb1df344e17df3b0505f605ae2ba983
Michael Czuba <czuba.mich@gmail.com>
2021-06-18 11:21:30 +0200

    Implement ApiHelper

---
+++ ApothecaryManager.WPF/Helpers/ApiHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Configuration;
+using ApothecaryManager.WPF.Models;
+
+namespace ApothecaryManager.WPF.Helpers
+{
+    class ApiHelper
+    {
+        public HttpClient ApiClient { get; set; }
+
+        public ApiHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["ApiAddress"];
+
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri(api);
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async Task Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+            });
+
+            using (HttpResponseMessage response = await ApiClient.PostAsync("/Identity/login", data))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                }
+            }
+        }
+    }
+}
---
+++ ApothecaryManager.WPF/Models/AuthenticatedUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApothecaryManager.WPF.Models
+{
+    class AuthenticatedUser
+    {
+        public int userId { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
