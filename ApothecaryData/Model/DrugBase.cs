f4f3d3847012784c76a20ac33bbd87d97efb2404
Bartosz Bożyczko <barybatle@gmail.com>
2021-06-24 23:26:45 +0200

    Update DrugBase.cs

---
+++ ApothecaryData/Model/DrugBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApothecaryManager.Data.Model
+{
+    [Table(name: "DrugsBase")]
+    public class DrugBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string QtyInStock { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string QtyOnOrder { get; set; }
+
+        public string Category { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string QuantityInPackage { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Price { get; set; }
+    }
+}
